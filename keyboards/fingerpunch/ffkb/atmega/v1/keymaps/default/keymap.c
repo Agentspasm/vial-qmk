5f279a96321660712aeaffaa652acfedaf36bc8a
Agentspasm <1995kirby@gmail.com>
2026-05-12 23:09:25 -0700

    Edit keymap to match bananakeeb and add engrammer

--- keyboards/fingerpunch/ffkb/atmega/v1/keymaps/default/keymap.c
+++ keyboards/fingerpunch/ffkb/atmega/v1/keymaps/default/keymap.c
@@ -3,14 +3,18 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY,
-    _COLEMAK,
     _LOWER,
     _RAISE,
+    _ENGRAMMER,
+    _ELOWER,
+    _ERAISE,
     _ADJUST
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define ELOWER MO(_ELOWER)
+#define ERAISE MO(_ERAISE)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,11 +35,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
 [_QWERTY] = LAYOUT_ffkb(
-  KC_ESC,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,             KC_BSPC,
-  KC_TAB,  LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN),  KC_QUOT,
-  KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
-                       KC_MUTE,      KC_BSLS,        KC_ENT,       LOWER,        RAISE,   KC_SPC,       KC_DEL,       LCTL(KC_BSPC),
-                                                                        LALT(KC_TAB)
+  KC_ESC,             KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,    KC_U,   KC_I,      KC_O,     KC_P,      KC_BSPC,
+  LT(LOWER,KC_TAB),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,    KC_J,   KC_K,      KC_L,     KC_SCLN,   KC_QUOT,
+  KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,    KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   SC_SENT,
+               KC_MUTE,     KC_LCTL,     KC_LGUI,     KC_LALT,    KC_SPC,      RAISE,      KC_DEL,     LCTL(KC_BSPC),
+                                                           LALT(KC_TAB)
+),
+
+//[_QWERTY] = LAYOUT_ffkb(
+//  KC_ESC,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,             KC_BSPC,
+//  KC_TAB,  LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN),  KC_QUOT,
+//  KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
+//                       KC_MUTE,      KC_BSLS,        KC_ENT,       LOWER,        RAISE,   KC_SPC,       KC_DEL,       LCTL(KC_BSPC),
+//                                                                        LALT(KC_TAB)
+//),
+
+[_ENGRAMMER] = LAYOUT_ffkb(
+    KC_TAB,               KC_B,   KC_Y,   KC_O,   KC_U,   KC_QUOT,      KC_SCLN,   KC_L,   KC_D,   KC_W,   KC_V,   KC_Z,
+    LT(KC_LCTL,KC_ESC),   KC_C,   KC_I,   KC_E,   KC_A,   KC_COMM,      KC_DOT,    KC_H,   KC_T,   KC_S,   KC_N,   KC_Q,
+    KC_LGUI,              KC_G,   KC_X,   KC_J,   KC_K,   KC_UNDS,      KC_SLSH,   KC_R,   KC_M,   KC_F,   KC_P,   KC_RALT,
+    KC_MUTE,   KC_LALT,   LT(KC_LSFT,KC_SPC),   LT(ERAISE,KC_DEL),      LT(ELOWER,KC_ENT)  LT(KC_LSFT,KC_SPC),   KC_RCTL, LCTL(KC_BSPC),
+                                                               LALT(KC_TAB)
 ),
 
 /* Colemak-dh
@@ -53,13 +73,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 // Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
-[_COLEMAK] = LAYOUT_ffkb(
-  KC_ESC,  KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,         KC_J,    KC_L,         KC_U,         KC_Y,         KC_SCLN,          KC_BSPC,
-  KC_TAB,  LCTL_T(KC_A), LGUI_T(KC_R), LALT_T(KC_S), LSFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N), RALT_T(KC_E), RGUI_T(KC_I), RCTL_T(KC_O),     KC_QUOT,
-  KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,         KC_K,    KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
-                       KC_MUTE,      KC_BSLS,        KC_ENT,       LOWER,        RAISE,   KC_SPC,       KC_DEL,       LCTL(KC_BSPC),
-                                                                        LALT(KC_TAB)
-),
+//[_COLEMAK] = LAYOUT_ffkb(
+//  KC_ESC,  KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,         KC_J,    KC_L,         KC_U,         KC_Y,         KC_SCLN,          KC_BSPC,
+//  KC_TAB,  LCTL_T(KC_A), LGUI_T(KC_R), LALT_T(KC_S), LSFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N), RALT_T(KC_E), RGUI_T(KC_I), RCTL_T(KC_O),     KC_QUOT,
+//  KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,         KC_K,    KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
+//                       KC_MUTE,      KC_BSLS,        KC_ENT,       LOWER,        RAISE,   KC_SPC,       KC_DEL,       LCTL(KC_BSPC),
+//                                                                        LALT(KC_TAB)
+//),
 
 
 /* Raise
@@ -76,15 +96,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          `------'        `--------------------'    `--------------------.        `------'
  */
 
+//[_RAISE] = LAYOUT_ffkb(
+//  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+//  KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+//  _______, _______, KC_PGDN, KC_PGUP, _______, _______,      _______, _______, _______, _______, _______, _______,
+//                    _______, _______, _______, _______,      _______, _______, _______, _______,
+//                                                      _______
+//),
+
 [_RAISE] = LAYOUT_ffkb(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-  _______, _______, KC_PGDN, KC_PGUP, _______, _______,      _______, _______, _______, _______, _______, _______,
-                    _______, _______, _______, _______,      _______, _______, _______, _______,
-                                                      _______
+ _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, 
+ RGB_MOD, RGB_SAI, RGB_VAI, RGB_HUI, RGB_SPI, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+ _______, RGB_SAD, RGB_VAD, RGB_HUD, RGB_SPD, _______, _______, _______, _______, _______, _______, _______,
+                   _______, _______, _______, _______, _______, _______, _______, _______
+                                                  _______,
 ),
-
-
 
 /* Raise
  *
@@ -100,13 +126,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          `------'        `--------------------'    `--------------------.        `------'
  */
 
-[_RAISE] = LAYOUT_ffkb(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-  _______, _______, KC_PGDN, KC_PGUP, _______, _______,      _______, _______, _______, _______, _______, _______,
-                    _______, _______, _______, _______,      _______, _______, _______, _______,
-                                                      _______
+[_ERAISE] = LAYOUT_ffkb(
+  KC_GRV,  KC_1,    KC_2,    KC_3,       KC_4,       KC_5,                  KC_6,               KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  _______, KC_MPLY, KC_VOLU, LCTL(KC_Z), LCTL(KC_C), KC_PRINT_SCREEN,       LCTL(KC_LEFT),      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, RCTL(KC_RGHT),
+  _______, KC_MUTE, KC_VOLD, LCTL(KC_Y), LCTL(KC_V), LCTL(LSFT(KC_T)),      LSFT(LCTL(KC_TAB)), KC_HOME, KC_PGUP, KC_PGDN, KC_END,  RCTL(KC_TAB),
+                    _______, _______,    _______,    _______,               _______,            _______, _______, _______,
+                                                                     _______
 ),
+
+
 /* Lower
  *
  * ,-----------------------------------------.             ,-----------------------------------------.
@@ -120,12 +148,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          | MUTE |        |   \  | Enter| LOWER|    | RAISE| Space| Del  |        | DELW |
  *          `------'        `--------------------'    `--------------------.        `------'
  */
+//[_LOWER] = LAYOUT_ffkb(
+//  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+//  _______, _______, _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+//  _______, KC_CAPS, _______, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, _______,
+//                    _______, _______, _______, _______,      _______, _______, _______, _______,
+//                                                      _______
+//),
+
 [_LOWER] = LAYOUT_ffkb(
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, _______, _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-  _______, KC_CAPS, _______, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, _______,
-                    _______, _______, _______, _______,      _______, _______, _______, _______,
-                                                      _______
+KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, 
+_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS,
+KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+                  _______, _______, _______, _______, _______, _______, _______, _______,
+                                                  _______
+),
+
+/* Lower
+ *
+ * ,-----------------------------------------.             ,-----------------------------------------.
+ * |      |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
+ * |------+------+------+------+------+------|   ,------.  |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |   |ALTTAB|  |      |   _  |   +  |   {  |   }  |      |
+ * |------+------+------+------+------+------|   `------'  |------+------+------+------+------+------|
+ * |      |  Caps|      |      |      |      |             |      |      |      |   |  |   "  |      |
+ * `-----------------------------------------'             `-----------------------------------------'
+ *          ,------.        ,--------------------.    ,--------------------.        ,------.
+ *          | MUTE |        |   \  | Enter| LOWER|    | RAISE| Space| Del  |        | DELW |
+ *          `------'        `--------------------'    `--------------------.        `------'
+ */
+[_ELOWER] = LAYOUT_ffkb(
+  KC_TILD, KC_EXLM,             KC_AT,               KC_HASH,           KC_DLR,              KC_PERC,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______, LSFT(KC_LEFT),       LSFT(KC_DOWN),       LSFT(KC_UP),       LSFT(KC_RGHT),       LALT(KC_LEFT),      KC_BSLS, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, _______,
+  _______, LSFT(LCTL(KC_LEFT)), LSFT(LCTL(KC_DOWN)), LSFT(LCTL(KC_UP)), LSFT(LCTL(KC_RGHT)), LALT(KC_UP),        KC_PIPE, KC_UNDS, KC_PLUS, LSFT(KC_LCBR),  LSFT(KC_RCBR), _______,
+                                _______,             _______,           _______,             _______,            _______, _______, _______, _______,
+                                                                                                          _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -143,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT_ffkb(
   _______, RGB_TOG, RGB_RMOD, RGB_MOD, _______, TO(_QWERTY),       KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,  _______,
-  _______, RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, TO(_COLEMAK),      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, _______,
+  _______, RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, TO(_ENGRAMMER),    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, _______,
   _______, RGB_SPD, RGB_HUD,  RGB_SAD, RGB_VAD, _______,           KC_F11,  KC_F12,  _______, _______,  QK_BOOT,  _______,
                      _______, _______, _______, _______,           _______, _______, _______, _______,
                                                          _______
@@ -152,6 +209,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _ELOWER, _ERAISE, _ADJUST);
 }
 
 #ifdef OLED_ENABLE
